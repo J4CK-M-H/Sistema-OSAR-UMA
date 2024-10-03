8af58a486055ec0fc1016cc373b7f361ee5fb135
Jack Meza Hinostroza <107339262+J4CK-M-H@users.noreply.github.com>
2024-10-02 19:54:18 -0500

    Delete api directory

--- api/helpers/generar-token.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generarJWT = (usuario = "", rol = "") => {
-  return jwt.sign({ usuario, rol }, "MY_SECRET_KEY", {
-    expiresIn: "1d",
-  });
-};
-
-export { generarJWT };
